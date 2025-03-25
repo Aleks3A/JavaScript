14b23527d4de580e6f10d640336fd126f683e251
Aleks3A <aaasupprog@gmail.com>
2025-03-25 11:31:16 +0200

    250325_2

--- counter.js
+++ counter.js
@@ -1,12 +1,26 @@
 let counter = 0;
-            function count() {
-                counter++;
-                document.querySelector('h1').innerHTML = counter
-                if (counter % 10 === 0) {
-        alert(`Count is now ${counter}`)
+
+// Перевірити, чи вже є значення у локальному сховищі
+if (!localStorage.getItem('counter')) {
+
+        // Якщо ні, встановити лічильник на нуль у локальному сховищі
+        localStorage.setItem('counter', 0);
     }
+                
+    
+function count() {
+                // Отримати значення лічильника з локального сховища
+    let counter = localStorage.getItem('counter');
+        counter++;
+                document.querySelector('h1').innerHTML = counter
+                // Зберегти лічильник у локальному сховищі
+    localStorage.setItem('counter', counter);
+
+                
 
             }
             document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = count;
+                document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+                document.querySelector('button').onclick = count;
+   
     });
